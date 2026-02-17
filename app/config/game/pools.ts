279e5f9c8c93a99afc5bc037136eb8f8ecc1bc47
Tegberen <tegberen@gmail.com>
2026-02-17 14:38:38 +0100

    typing changes for the vibes

--- app/config/game/pools.ts
+++ app/config/game/pools.ts
@@ -19,7 +19,6 @@ export const UniquePool = new Array<PkmProposition>(
   Pkm.UXIE,
   Pkm.MESPRIT,
   Pkm.AZELF,
-  PkmDuo.LATIOS_LATIAS,
   Pkm.LAPRAS,
   Pkm.ABSOL,
   Pkm.SPIRITOMB,
@@ -172,8 +171,6 @@ export const LegendaryPool = new Array<PkmProposition>(
   Pkm.NECROZMA,
   Pkm.XURKITREE,
   Pkm.NIHILEGO,
-  Pkm.PHEROMOSA,
-  Pkm.BUZZWOLE,
   Pkm.TORNADUS,
   Pkm.THUNDURUS,
   Pkm.LANDORUS,
@@ -200,5 +197,7 @@ export const LegendaryPool = new Array<PkmProposition>(
   Pkm.MANAPHY,
   Pkm.CHI_YU,
   Pkm.TYPE_NULL,
-  Pkm.BLACEPHALON
+  Pkm.BLACEPHALON,
+  PkmDuo.LATIOS_LATIAS,
+  PkmDuo.BUZZWOLE_PHEROMOSA
 )
