54975bcf0bfff535caa0072bb814acd89249a5ac
Tegberen <tegberen@gmail.com>
2026-03-18 15:47:05 +0100

    change mega charizard x

--- app/core/effects/passives.ts
+++ app/core/effects/passives.ts
@@ -1561,6 +1561,11 @@ export const PassiveEffects: Partial<
         attacker.count.amuletCoinCount += 1
       }
     })
+  ],
+  [Passive.CHARIZARD]: [
+    new OnKillEffect(({ attacker }) => {
+      attacker.addStack()
+    })
   ]
   
 }
