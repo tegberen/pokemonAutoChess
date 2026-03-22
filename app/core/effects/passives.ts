7c22eb03ad8b0fa6a2ea650598539e5da39d8b3c
Tegberen <tegberen@gmail.com>
2026-03-22 01:50:17 +0100

    balance megas

--- app/core/effects/passives.ts
+++ app/core/effects/passives.ts
@@ -1695,7 +1695,7 @@ export const PassiveEffects: Partial<
           const blocked = damageBeforeReduction - damage
           if (blocked <= 0) return
           this.accumulated += blocked
-          const newStacks = Math.floor(this.accumulated / 500)
+          const newStacks = Math.floor(this.accumulated / 200)
           if (newStacks > this.stacksGiven) {
             for (let i = this.stacksGiven; i < newStacks; i++) {
               pokemon.addStack()
@@ -1712,7 +1712,7 @@ export const PassiveEffects: Partial<
       constructor() {
         super(
           (pokemon) => {
-            const newStacks = Math.floor(pokemon.speed / 150)
+            const newStacks = Math.floor(pokemon.speed / 100)
             if (newStacks > this.stacksGiven) {
               for (let i = this.stacksGiven; i < newStacks; i++) {
                 pokemon.addStack()
@@ -1731,7 +1731,7 @@ export const PassiveEffects: Partial<
       constructor() {
         super(
           (pokemon) => {
-            if (pokemon.shield>= 150) {
+            if (pokemon.shield>= 100) {
               pokemon.addStack()
               pokemon.effectsSet.delete(this)
             }
@@ -1801,6 +1801,7 @@ export const PassiveEffects: Partial<
       }
     }),
     () => new class extends OnDamageReceivedEffect {
+      if (!pokemon.player) return
       accumulated = 0
       stacksGiven = 0
       constructor() {
@@ -1808,7 +1809,7 @@ export const PassiveEffects: Partial<
           const blocked = damageBeforeReduction - damage
           if (blocked <= 0) return
           this.accumulated += blocked
-          const newStacks = Math.floor(this.accumulated / 300)
+          const newStacks = Math.floor(this.accumulated / 100)
           if (newStacks > this.stacksGiven) {
             for (let i = this.stacksGiven; i < newStacks; i++) {
               pokemon.addStack()
