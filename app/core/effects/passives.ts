bf12e592dcdf93ef881be8ad8274f0fccdc5bb68
Tegberen <tegberen@gmail.com>
2026-03-24 08:49:11 +0100

    buff gengar

--- app/core/effects/passives.ts
+++ app/core/effects/passives.ts
@@ -1908,6 +1908,13 @@ export const PassiveEffects: Partial<
       attacker.addStack()
     })
   ],
+  [Passive.GENGAR]: [
+    new OnKillEffect(({ attacker }) => {
+      if (attacker.isGhostOpponent) return
+      if (!attacker.player) return
+      attacker.addStack()
+    })
+  ],
   [Passive.MEGA_ALAKAZAM]: [
     new OnHitEffect(({ attacker, target }) => {
       if (chance(0.3, attacker)) {
