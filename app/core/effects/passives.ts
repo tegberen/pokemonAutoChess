7373527cf2f87243a10777d368890e7d9fe43484
Tegberen <tegberen@gmail.com>
2026-03-28 06:14:06 +0100

    fix

--- app/core/effects/passives.ts
+++ app/core/effects/passives.ts
@@ -1750,7 +1750,7 @@ export const PassiveEffects: Partial<
       if (!pokemon.player) return
       if (pokemon.isGhostOpponent) return
       pokemon.addStack()
-      if (weather === Weather.RAIN) {
+      if (pokemon.simulation.weather === Weather.RAIN) {
         pokemon.addStack()
       }
     }, Passive.SLOWBRO)
