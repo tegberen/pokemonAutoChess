7e2b133331f30a4724248bba8ff04b8c23095ef4
Tegberen <tegberen@gmail.com>
2026-03-28 04:41:41 +0100

    make scribbles selectable

--- app/core/gadgets.ts
+++ app/core/gadgets.ts
@@ -19,42 +19,42 @@ const BAG: Gadget = {
   name: "gadget.bag",
   description: "gadget.bag_desc",
   icon: "school-bag",
-  levelRequired: 1
+  levelRequired: 0
 }
 
 const TEAM_PLANNER: Gadget = {
   name: "gadget.team_planner",
   description: "gadget.team_planner_desc",
   icon: "team-builder",
-  levelRequired: 2
+  levelRequired: 0
 }
 
 const JUKEBOX: Gadget = {
   name: "gadget.jukebox",
   description: "gadget.jukebox_desc",
   icon: "compact-disc",
-  levelRequired: 5
+  levelRequired: 0
 }
 
 const SYNERGY_WHEEL: Gadget = {
   name: "gadget.synergy_wheel",
   description: "gadget.synergy_wheel_desc",
   icon: "synergy-wheel",
-  levelRequired: 10
+  levelRequired: 0
 }
 
 const GAMEBOY: Gadget = {
   name: "gadget.gameboy",
   description: "gadget.gameboy_desc",
   icon: "gameboy",
-  levelRequired: 20
+  levelRequired: 0
 }
 
 const POKEGUESSER: Gadget = {
   name: "gadget.pokeguesser",
   description: "gadget.pokeguesser_desc",
   icon: "pokeguesser",
-  levelRequired: 30
+  levelRequired: 0
 }
 
 const BOT_BUILDER: Gadget = {
@@ -69,7 +69,7 @@ const TIER_LIST_MAKER: Gadget = {
   name: "gadget.tier_list_maker",
   description: "gadget.tier_list_maker_desc",
   icon: "tier-list",
-  levelRequired: 50
+  levelRequired: 0
 }
 
 export const GADGETS = {
