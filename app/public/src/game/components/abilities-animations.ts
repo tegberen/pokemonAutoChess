b43c5a582c19adc3fa0b132d9a01cbab63221966
Tegberen <tegberen@gmail.com>
2026-03-29 20:22:44 +0200

    rebase/fix-pool-distribution

--- app/public/src/game/components/abilities-animations.ts
+++ app/public/src/game/components/abilities-animations.ts
@@ -1836,7 +1836,7 @@ export const AbilitiesAnimations: {
   }),
   [Ability.DARK_NOVA]: skyfall({
     ability: Ability.SUNSTEEL_STRIKE,
-    scale: 0.5,
+    scale: 1,
     duration: 500,
     tint: 0xffe8ff,
     hitAnim: onCaster({
