a76c7b8a97866d7a37b01d84b29ac68493f3fdf4
Tegberen <tegberen@gmail.com>
2026-04-03 15:46:38 +0200

    buff xurkitree

--- app/models/colyseus-models/pokemon.ts
+++ app/models/colyseus-models/pokemon.ts
@@ -14298,8 +14298,8 @@ export class Xurkitree extends Pokemon {
   ])
   rarity = Rarity.LEGENDARY
   stars = 3
-  hp = 200
-  atk = 15
+  hp = 250
+  atk = 20
   speed = 52
   def = 6
   speDef = 6
