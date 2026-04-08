cea334a7dd9ffa5bc6bc17ef7c8bd5789e012fcc
Tegberen <tegberen@gmail.com>
2026-04-08 13:31:49 +0200

    add fluffy tail
    just slamming fluffy tail = 20% dodge chance (similiar to olive dish,
    but you use one item slot)

--- app/core/effects/items.ts
+++ app/core/effects/items.ts
@@ -1701,4 +1701,12 @@ export const ItemEffects: { [i in Item]?: (Effect | (() => Effect))[] } = {
       }
     })
   ],
+  [Item.FLUFFY_TAIL]: [
+    new OnItemGainedEffect((pokemon) => {
+      pokemon.addDodgeChance(0.1, pokemon, 0, false)
+    }),
+    new OnItemRemovedEffect((pokemon) => {
+      pokemon.addDodgeChance(-0.1, pokemon, 0, false)
+    })
+  ]
 }
