2793a174b9868b7dfb1c7d7e476c1cf394fdfa91
Tegberen <tegberen@gmail.com>
2026-04-20 13:20:02 +0200

    totodile tent and dialog

--- app/public/dist/client/sw.js
+++ app/public/dist/client/sw.js
@@ -1,7 +1,7 @@
 /* Change this cache name every time you want to force players 
   to invalidate their cache and download all assets again */
 
-const CACHE_NAME = "CACHE v6.9.0.828"
+const CACHE_NAME = "CACHE v6.9.0.829"
 
 // Cache-first strategy
 const cacheFirst = (event) => {
