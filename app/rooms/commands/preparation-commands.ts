2d323eded632005eaa736e173bef5ac18b082183
Tegberen <tegberen@gmail.com>
2026-04-25 22:14:15 +0200

    clarify description iron moth

--- app/rooms/commands/preparation-commands.ts
+++ app/rooms/commands/preparation-commands.ts
@@ -63,7 +63,7 @@ export class OnJoinCommand extends Command<
       }
       this.state.addMessage({
         authorId: "server",
-        payload: "Remember to change from >>Regular Mix<< to >>Beginner or Medium<< for the first few games. (っᵔ◡ᵔ)っ"
+        payload: "Remember to change >Add Bot< from >>Regular Mix<< to >>Beginner or Medium<< for the first few games. (っᵔ◡ᵔ)っ"
       })
 
       const u = await UserMetadata.findOne({ uid: auth.uid })
