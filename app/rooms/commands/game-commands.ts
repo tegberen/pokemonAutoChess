13731db1a13926f20a22f20be0f0d70bb2905041
Tegberen <tegberen@gmail.com>
2026-06-12 14:02:56 +0200

    double up: bugs sending items
    - remove items on succeeded sent
    - clear items, to avoid 4 items wonderbox

--- app/rooms/commands/game-commands.ts
+++ app/rooms/commands/game-commands.ts
@@ -314,11 +314,6 @@ function sendPokemonToPartner(
 
   // Remove from sender's board
   sender.board.delete(pokemon.id)
-  const itemsToReturn = schemaValues(pokemon.items)
-  pokemon.removeItems(itemsToReturn, sender)
-  itemsToReturn.forEach((item) => {
-    sender.items.push(item)
-  })
   sender.updateSynergies()
   sender.boardSize = room.getTeamSize(sender.board)
 
@@ -338,6 +333,14 @@ function sendPokemonToPartner(
       }
       return
     }
+    // success: remove items
+    const itemsToReturn = schemaValues(pokemon.items)
+    pokemon.removeItems(itemsToReturn, sender)
+    itemsToReturn.forEach((item) => {
+      sender.items.push(item)
+    })
+    sender.updateSynergies()
+
     pokemon.positionX = freeX
     pokemon.positionY = 0
     partner.board.set(pokemon.id, pokemon)
@@ -1288,11 +1291,8 @@ export class OnUpdateCommand extends Command<
       )
       reinforcement.sourcePlayer = winnerPlayer
       // e.g. comfey will be attached here
-      entity.items.forEach(item => {
-        if (!reinforcement.items.has(item)) {
-          reinforcement.items.add(item)
-        }
-      })
+      reinforcement.items.clear()
+      entity.items.forEach(item => reinforcement.items.add(item))
       // negative status effects are not carried over, therefore do not bring over (most) positive status effects
       // positive effects we ignore: spikeArmor, magigBounce, reflect, pokerus, rage
 
